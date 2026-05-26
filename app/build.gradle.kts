9ff1675cf5a12a9039081a879dae6bd60e93ccad
tsubuanpn <171699186+tsubuanpn@users.noreply.github.com>
2026-05-26 19:07:05 +0900

    revert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.notiontasks.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
